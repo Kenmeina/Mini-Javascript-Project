876d0f419d17abc6ef6d1272a5ceb3348bfcc693
Hasiff Jazlan <hasiffjazlan@gmail.com>
2023-10-28 13:03:27 +0800

    First Commit

---
+++ project_1.js
@@ -0,0 +1,33 @@
+let num1 = 10;
+let num2 = 5;
+
+// Create constants
+const pi = 3.14159;
+
+// Arithmetic operations
+let additionResult = num1 + num2;
+let subtractionResult = num1 - num2;
+let multiplicationResult = num1 * num2;
+let divisionResult = num1 / num2;
+
+// Increment and decrement operators
+let incrementResult = num1++; // Post-increment
+let decrementResult = num2--; // Post-decrement
+
+// Assignment operators
+let assignmentNum = 5;
+assignmentNum += 2; // assignmentNum is now 7
+assignmentNum -= 3; // assignmentNum is now 4
+assignmentNum *= 2; // assignmentNum is now 8
+assignmentNum /= 4; // assignmentNum is now 2
+
+// Comparison operators
+let isEqual = (num1 === num2); // Check if num1 and num2 are equal
+let isNotEqual = (num1 !== num2); // Check if num1 and num2 are not equal
+let isGreater = (num1 > num2); // Check if num1 is greater than num2
+let isLess = (num1 < num2); // Check if num1 is less than num2
+
+// Logical operators
+let logicalAnd = (num1 > 0 && num2 < 10); // Logical AND
+let logicalOr = (num1 > 0 || num2 > 10); // Logical OR
+let logicalNot = !(num1 === num2); // Logical NOT
